e6fe3fa527364065e6e9822166c17f4d871d5053
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-01-31 01:36:05 +0530

    feat: added solana support

--- index.js
+++ index.js
@@ -13,38 +13,64 @@ const client = new Client({
   ],
 });
 
+const { PublicKey } = require("@solana/web3.js");
+
 const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const guildId = process.env.guildId;
+const channelId = process.env.channelId;
+
 const db = require("./db");
 
+function isValidSolanaAddress(address) {
+  try {
+    new PublicKey(address);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 client.on(Events.MessageCreate, async (interaction) => {
-  await db.connect();
-  // console.log(interaction)
-  console.log(interaction.content);
-  //Get the contents of the message
-  let content = interaction.content;
+  if (interaction.guildId === guildId && interaction.channelId === channelId) {
+    try {
+      await db.connect();
+      console.log(interaction.content);
+      let content = interaction.content;
+      content.trim();
 
-  let address = await db.get("whitelisted_wallets");
-  address = await JSON.parse(address);
-  // console.log(address);
+      let address = await db.get("whitelisted_wallets");
+      // console.log(address);
+      address = await JSON.parse(address);
 
-  if (!address) address = [];
-  // Push the wallet to be whitelisted
-  if (
-    content.startsWith("0x") &&
-    content.length == 42 &&
-    !address.includes(content)
-  )
-    address.push(content);
+      if (!address) address = [];
+      
+      console.log(isValidSolanaAddress(content));
 
-  await db.set("whitelisted_wallets", JSON.stringify(address));
-  address = await db.get("whitelisted_wallets");
-  // console.log(address);
+      if (
+        (content.startsWith("0x") &&
+          content.length == 42 &&
+          !address.includes(content)) ||
+        isValidSolanaAddress(content)
+      ) {
+        address.push(content);
 
-  interaction.react("👍");
-  await db.disconnect();
+        await db.set("whitelisted_wallets", JSON.stringify(address));
+        address = await db.get("whitelisted_wallets");
+        // console.log(address);
+
+        interaction.react("👍");
+      } else {
+        interaction.react("👎");
+      }
+      await db.disconnect();
+    } catch (error) {
+      interaction.react("❗");
+      console.log(error);
+    }
+  }
 });
 
 client.on(Events.ClientReady, () => {
